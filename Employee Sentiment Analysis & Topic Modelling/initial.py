99fcdb51d5cd6c5deb2cdccdb16ac3f698b1303a
KANNIAPPAN <lingeshwaran.kanniappan@capgemini.com>
2019-10-31 15:04:03 +0000

    Reading of Files

--- Employee Sentiment Analysis & Topic Modelling/initial.py
+++ Employee Sentiment Analysis & Topic Modelling/initial.py
@@ -2,3 +2,10 @@ import pandas as pd
 path = 'data/train.csv'
 df = pd.read_csv(path)
 print(df.head(5))
+
+for col in df.columns:
+    print(f'Column {col} contains: {len(df[col].unique())} values')
+
+ratings = df.columns[10:]
+for rat in ratings:
+    print(f"{rat} : {sorted(df[rat].unique())}")
