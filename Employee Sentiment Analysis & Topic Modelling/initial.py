73f13ffb78a0b184a8612ff2beffab8d9e61b268
KANNIAPPAN <lingeshwaran.kanniappan@capgemini.com>
2019-11-04 15:33:10 +0000

    Update main function

--- Employee Sentiment Analysis & Topic Modelling/initial.py
+++ Employee Sentiment Analysis & Topic Modelling/initial.py
@@ -1,11 +1,16 @@
-import pandas as pd
-path = 'data/train.csv'
-df = pd.read_csv(path)
-print(df.head(5))
+def initial(): 
+    import pandas as pd
+    path = 'data/train.csv'
+    df = pd.read_csv(path)
+    print(df.head(5))
 
-for col in df.columns:
-    print(f'Column {col} contains: {len(df[col].unique())} values')
+    for col in df.columns:
+        print(f'Column {col} contains: {len(df[col].unique())} values')
 
-ratings = df.columns[10:]
-for rat in ratings:
-    print(f"{rat} : {sorted(df[rat].unique())}")
+    ratings = df.columns[10:]
+    for rat in ratings:
+        print(f"{rat} : {sorted(df[rat].unique())}")
+
+if __name__ == "__main__":
+    initial()
+    return True
